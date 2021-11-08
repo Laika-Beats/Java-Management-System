72be01699835374025a1dd165dfd97d036134fe4
Laika-Beats <laikabeatsdenver@gmail.com>
2021-11-08 13:08:54 -0700

    @Column

--- src/main/java/net/javaguides/springboot/model/Employee.java
+++ src/main/java/net/javaguides/springboot/model/Employee.java
@@ -1,5 +1,6 @@
 package net.javaguides.springboot.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,8 +14,14 @@ public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@Column(name = "first_name")
 	private String firstName;
+	
+	@Column(name = "last_name")
 	private String lastName;
+	
+	@Column(name = "email")
 	private String email;
 	
 	
