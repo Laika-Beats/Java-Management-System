e99ccf02e7294ffab70d2ee14e8d3422beb964a8
Laika-Beats <laikabeatsdenver@gmail.com>
2021-11-08 13:36:50 -0700

    resource not found exception + save employee

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -1,16 +1,48 @@
 package net.javaguides.springboot.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguides.springboot.exception.ResourceNotFoundException;
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
 	// GET employees
+	@GetMapping("employees")
+	public List<Employee> getAllEmployee(){
+		return this.employeeRepository.findAll();
+	}
 	
 	// GET employees by ID
+	@GetMapping("employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId)
+		throws ResourceNotFoundException {
+		Employee employee = employeeRepository.findById(employeeId)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+		return ResponseEntity.ok().body(employee);
+	}
+	
 	
 	// SAVE employee
+	@PostMapping("employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return this.employeeRepository.save(employee);
+	}
 	
 	// UPDATE employee
 	
